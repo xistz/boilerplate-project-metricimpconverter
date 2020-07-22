d055bdce6e8f53bddd67d7524e21fa03a8776811
Jonathan Chan <tmxistz@gmail.com>
2020-07-22 12:10:15 +0900

    completed getNum

--- controllers/convertHandler.js
+++ controllers/convertHandler.js
@@ -1,52 +1,66 @@
 /*
-*
-*
-*       Complete the handler logic below
-*       
-*       
-*/
+ *
+ *
+ *       Complete the handler logic below
+ *
+ *
+ */
 
 function ConvertHandler() {
-  
-  this.getNum = function(input) {
-    var result;
-    
-    return result;
+  this.re = /^(?<num>(\d+\.)?\d+(\/\d+)?)?\s?(?<unit>gal|L|lbs|kg|mi|km)$/;
+
+  this.getNum = function (input) {
+    const matches = input.match(this.re);
+
+    if (!matches) {
+      return 'invalid number';
+    }
+
+    const num = matches[1];
+
+    if (!num) {
+      return 1;
+    }
+
+    return matches[1];
   };
-  
-  this.getUnit = function(input) {
-    var result;
-    
-    return result;
+
+  this.getUnit = function (input) {
+    const matches = input.match(this.reUnit);
+
+    if (!matches) {
+      return 'invalid unit';
+    }
+
+    return matches[0];
   };
-  
-  this.getReturnUnit = function(initUnit) {
+
+  this.getReturnUnit = function (initUnit) {
     var result;
-    
+
     return result;
   };
 
-  this.spellOutUnit = function(unit) {
+  this.spellOutUnit = function (unit) {
     var result;
-    
+
     return result;
   };
-  
-  this.convert = function(initNum, initUnit) {
+
+  this.convert = function (initNum, initUnit) {
     const galToL = 3.78541;
     const lbsToKg = 0.453592;
     const miToKm = 1.60934;
     var result;
-    
+
     return result;
   };
-  
-  this.getString = function(initNum, initUnit, returnNum, returnUnit) {
+
+  this.getString = function (initNum, initUnit, returnNum, returnUnit) {
     var result;
-    
+
     return result;
   };
-  
 }
 
 module.exports = ConvertHandler;
